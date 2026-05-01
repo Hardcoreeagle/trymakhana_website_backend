bdc41c55d0be8f271f7dabfbc10b8cb15b24bd4b
Hardcoreeagle <153204341+Hardcoreeagle@users.noreply.github.com>
2026-05-01 12:36:26 +0530

    CORS update

--- src/index.js
+++ src/index.js
@@ -12,9 +12,6 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'https://trymaakhanastores.com',
-  'http://trymaakhanastores.com',
-  'https://www.trymaakhanastores.com',
-  'http://www.trymaakhanastores.com',
 ]
 
 app.use(cors({
